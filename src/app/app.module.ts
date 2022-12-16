69e595891e2f435605ebbb36b652216d5dff8d2d
Edis <edy_l20@abv.bg>
2022-12-16 21:36:33 +0200

    Changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppComponent } from './app.component';
+import { DeliveryComponent } from './components/delivery/delivery.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SaladsComponent } from './components/salads/salads.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { DessertsComponent } from './components/desserts/desserts.component';
+import { FamousComponent } from './components/famous/famous.component';
+import { RateDialog } from './dialogs/rate-dialog.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationComponent,
+    DeliveryComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuComponent,
+    RateDialog,
+    SaladsComponent,
+    AboutUsComponent,
+    DessertsComponent,
+    FamousComponent,
+    OffersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgbModule,
+    NgxMaterialTimepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/reservation.model.ts
@@ -0,0 +1,12 @@
+export interface ReservationModel{
+    date: string;
+    time: string;
+    numberPeople: number;
+    neighborhood: string;
+    name: string;
+    email: string;
+    phone: number;
+    comment: string;
+    agree: boolean;
+  }
+  
